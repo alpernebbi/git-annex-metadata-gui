7bf5fd7dfee8b09ad5e73b39ff7289dcd49fca92
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-11-08 22:25:25 +0300

    Split creating things into methods

--- git-annex-metadata-gui.py
+++ git-annex-metadata-gui.py
@@ -20,6 +20,11 @@ class MainWindow(QMainWindow):
         self.resize(800, 600)
         self.setWindowTitle('Git Annex Metadata Editor')
 
+        self.create_actions()
+        self.create_menus()
+        self.create_statusbar()
+
+    def create_actions(self):
         self.exit_action = QAction(
             "Exit", self,
             shortcut="Ctrl+Q",
@@ -27,9 +32,11 @@ class MainWindow(QMainWindow):
             triggered=self.close,
         )
 
+    def create_menus(self):
         self.file_menu = self.menuBar().addMenu('&File')
         self.file_menu.addAction(self.exit_action)
 
+    def create_statusbar(self):
         self.statusBar().showMessage('Ready')
 
 if __name__ == '__main__':
