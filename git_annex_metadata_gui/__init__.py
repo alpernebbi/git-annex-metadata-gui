090caab18210e6a710d4236fed1c21da4f55c372
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2017-07-24 13:23:40 +0300

    Display main window when run

---
+++ git_annex_metadata_gui/__init__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# Git-Annex-Metadata-Gui
+# Copyright (C) 2017 Alper Nebi Yasak
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+
+from .main_window import Ui_MainWindow
+
+app = None
+
+def main():
+    global app
+    app = QtWidgets.QApplication(sys.argv)
+
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+
+    return app.exec_()
+
+if __name__ == "__main__":
+    main()
