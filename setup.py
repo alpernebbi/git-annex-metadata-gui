200d1153aaa09c4ab63cc70943ddd63a16afc5de
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2017-07-24 12:53:22 +0300

    Update license header in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-# Git-Annex-Metadata-Gui Setup
-# Copyright (C) 2016 Alper Nebi Yasak
+# Git-Annex-Metadata-Gui
+# Copyright (C) 2017 Alper Nebi Yasak
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
